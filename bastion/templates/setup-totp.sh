2bea8ae50a3aa9aaef2b61356e1b866e8a5a9054
Jon DeCamp <jjdecamp@MacBook-Pro.local>
2021-03-09 17:07:20 -0800

    Initial Commit

---
+++ bastion/templates/setup-totp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+oathdir="${HOME}/.oath"
+oathfile="${oathdir}/opc.oath"
+ 
+sudo grep 'INITIAL SETUP' $oathfile > /dev/null 2>&1
+ 
+if [[ $? -eq 0 ]]
+then
+    hex=`openssl rand -hex 20`
+ 
+    echo "HOTP/T30/6 `whoami` - $hex" > ${oathfile}.$$
+ 
+    sudo cp ${oathfile}.$$ ${oathfile}
+    sudo chown root ${oathfile}
+    sudo chmod 600 ${oathfile}
+    /bin/gen-oath-safe opc totp $hex
+ 
+    echo "*** USE THIS CODE OR LOSE ACCESS TO THIS HOST ***"
+    rm ${oathfile}.$$
+ fi
